2e8b644c6dcf375f4224cc9cc291771f9c158905
lovelyhyeony <dmlgus1222@naver.com>
2021-01-02 02:41:25 +0900

    facebook clone coding project

---
+++ react-facebook-clone/src/firebase copy.js
@@ -0,0 +1,19 @@
+import firebase, { firestore } from "firebase";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
